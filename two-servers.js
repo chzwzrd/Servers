c1148f3a76f25d392b317dd96411729ee867b8c7
Melodie Chi <Melodie@dhcp-v077-145.mobile.uci.edu>
2017-12-09 11:17:44 -0800

    finished

--- two-servers.js
+++ two-servers.js
@@ -3,10 +3,32 @@
 // Using the previous example as a guide, create an app that has two web servers.
 var http = require('http');
 // One that listens on port 7000 and one that listens on port 7500.
-var PORT1 = 7000;
-var PORT2 = 7500;
+var posPort = 7000;
+var negPort = 7500;
+
 // The one listening on port 7000 will always tell the user something good about themselves.
+function posRequest(req, res) {
+    var pos = [`You're awesome!`, `Rockin that outfit B]`, `Rad, dude =]`, `You're great.`];
+    var random = pos[Math.floor(Math.random() * pos.length)];
+    res.end(random);
+}
 // The one listening on 7500 will always tell the user something bad about themselves.
+function negRequest(req, res) {
+    var neg = [`You don't look so good today...`, `Hmm..bad hair day?`, `Need some gum? Cough..`, `Bat in the cave...`];
+    var random = neg[Math.floor(Math.random() * neg.length)];
+    res.end(random);
+}
+
+var posServer = http.createServer(posRequest);
+var negServer = http.createServer(negRequest);
+
+posServer.listen(posPort, function() {
+    console.log(`listening on: http://localhost:${posPort}`);
+});
+
+negServer.listen(negPort, function(){
+    console.log(`listening on: http://localhost:${negPort}`);
+});
 // Make sure you create a Github repo and commit this code!
 // Bonus
 
